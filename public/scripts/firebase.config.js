54f2c3025bd054918019a5f635a9707d0ce2c145
unknown <faraazm>
2018-07-04 19:16:08 -0400

    Initial Commit

---
+++ public/scripts/firebase.config.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBbqZDjSejPcjTB3eaGTJrYpclXl6qcDys",
+    authDomain: "test-project-5a877.firebaseapp.com",
+    databaseURL: "https://test-project-5a877.firebaseio.com",
+    projectId: "test-project-5a877",
+    storageBucket: "test-project-5a877.appspot.com",
+    messagingSenderId: "109695302408"
+  };
+  firebase.initializeApp(config);
